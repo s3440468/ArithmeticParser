eef8cb34a0a087166284d60d5cebc4020d989602
s3440468 <s3440468@student.rmit.edu.au>
2016-10-02 01:06:31 +1000

    Add files via upload

---
+++ imagene/arithmeticParser/parserExceptions/IncorrectVariablesException.java
@@ -0,0 +1,20 @@
+/****************************
+ * Arithmetic Parser        *
+ *                          *
+ * Created by Andrew Sanger *
+ * for                      *
+ * Programming Project 1    *
+ * SP3 2016                 *
+ ****************************/
+
+package imagene.arithmeticParser.parserExceptions;
+
+// This Exception is thrown when there are either too many or not enough
+// variables in an argument. eg sine needs 1 argument, if there are 0 or
+// 2 arguments, then this exception is thrown.
+@SuppressWarnings("serial")
+public class IncorrectVariablesException extends Exception{
+	public IncorrectVariablesException(String message) {
+		super(message);
+	}
+}
---
+++ imagene/arithmeticParser/parserExceptions/InvalidArgumentException.java
@@ -0,0 +1,21 @@
+/****************************
+ * Arithmetic Parser        *
+ *                          *
+ * Created by Andrew Sanger *
+ * for                      *
+ * Programming Project 1    *
+ * SP3 2016                 *
+ ****************************/
+
+package imagene.arithmeticParser.parserExceptions;
+
+// This Exception is thrown when the supplied argument string is invalid
+// As an example, instead of receiving sin as input, it may get zin or si
+// this exception would then be thrown.
+
+@SuppressWarnings("serial")
+public class InvalidArgumentException extends Exception {
+	public InvalidArgumentException(String message) {
+		super(message);
+	}
+}
